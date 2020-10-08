9b49dd12a0d1a8894b929865d22797bd35cd45ff
Mohammed Asif <iamkmasee@gmail.com>
2020-10-08 07:16:10 -0400

    initial commit

--- src/components/Icon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ({ className, icon = "", fontSize = "1.5rem" }) {
-  return (
-    <div className={["d-inline", className].join(" ")}>
-      <i className={["fas", icon].join(" ")} style={{ fontSize: fontSize }}></i>
-    </div>
-  );
-}
