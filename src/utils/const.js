ee2950ea26ff2feaac9066da169bef38e9f52017
Mohammed Asif <iamkmasee@gmail.com>
2020-10-10 19:59:43 -0400

    sync

--- src/utils/const.js
+++
@@ -1,7 +0,0 @@
-export const COLOR_PRIMARY = "#2b5162";
-export const COLOR_SECONDARY = "rgb(255, 255, 255)";
-export const COLOR_DEFAULT = "rgb(190, 190, 190)";
-export const COLOR_DARK = "rgb(8, 8, 8)";
-export const COLOR_PRIMARY_DARK = "#232C3B";
-export const COLOR_PRIMARY_LIGHT = "#546B86";
-export const COLOR_PRIMARY_LIGHTER = "#546b86b0";
