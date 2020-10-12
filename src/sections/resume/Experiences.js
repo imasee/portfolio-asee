f7ac3acc0f15440382dbf4c530bc2bee121663ef
iamasee <iamkmasee@gmail.com>
2020-10-12 00:33:23 -0400

    added service functions

--- src/sections/resume/Experiences.js
+++ src/sections/resume/Experiences.js
@@ -22,7 +22,7 @@ export default function () {
       </div> */}
       {
         educations.map(
-          e => {
+          (e, i) => {
             return <TimelineItem
               institution={e.company}
               location={e.location}
@@ -30,6 +30,7 @@ export default function () {
               dateTo={e.dateTo}
               datefrom={e.dateFrom}
               icon="fa-briefcase"
+              key={i}
             >
 
             </TimelineItem>
